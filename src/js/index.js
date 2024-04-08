f9fadceddf9bf04f7c1c9ae881bc476dea6c5816
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-04-08 13:07:02 -0500

    feat: carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,14 @@
-function Data(){
-    return {
-        toggle: window.innerWidth > 845, // Inicializar toggle basado en el ancho de la ventana
-
-        Start() {
-            if (window.innerWidth <= 845) { // Si la pantalla es menor o igual a 768px
-                this.toggle = false; // Sobrescribir toggle a false
-            }
-        },
-        SetToggle(){
-            this.toggle =!this.toggle;
-        },
-        GetToggle(){
-            return this.toggle;
-        }
-    }
-}
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 15,
+      stretch: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadows: false,
+    },
+    loop: true,
+    });
