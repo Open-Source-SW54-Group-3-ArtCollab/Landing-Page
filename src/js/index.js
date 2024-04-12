55a34ec3f76fde4b723541a87acb4ac00309ece7
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-12 17:10:23 -0500

    feat: merge with feature/footer and feature/hero

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
 function Data(){
     return {
-        toggle: window.innerWidth > 845, // Inicializar toggle basado en el ancho de la ventana
-
+        toggle: window.innerWidth > 768, // Inicializar toggle basado en el ancho de la ventana
+  
+        Start() {
+            if (window.innerWidth <= 768) { // Si la pantalla es menor o igual a 768px
+                toggle: window.innerWidth > 845; // Inicializar toggle basado en el ancho de la ventana
+            }
+          },
+  
         Start() {
             if (window.innerWidth <= 845) { // Si la pantalla es menor o igual a 768px
                 this.toggle = false; // Sobrescribir toggle a false
@@ -14,4 +20,19 @@ function Data(){
             return this.toggle;
         }
     }
-}
+  }
+  
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 15,
+      stretch: 10,
+      depth: 300,
+      modifier: 1,
+      slideShadows: false
+    },
+    loop: true,
+    });
